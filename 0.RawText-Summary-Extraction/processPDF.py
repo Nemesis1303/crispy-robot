11c632627fc957135866c8269e736ecf45e2101a
Saúl Blanco Fortes <sblanco@kumo01.tsc.uc3m.es>
2024-03-08 12:58:04 +0100

    integrado con lorena
    Signed-off-by: Saúl Blanco Fortes <sblanco@kumo01.tsc.uc3m.es>

--- 0.RawText-Summary-Extraction/processPDF.py
+++ 0.RawText-Summary-Extraction/processPDF.py
@@ -2,10 +2,15 @@ import argparse
 import glob
 import time 
 import multiprocessing
+from datetime import datetime
+import json
+import time
+import fitz
 import pathlib
+
+#Propias:
 from src.pdf_parser import PDFParser
 from src.summarizer import Summarizer
-import fitz
 from src.multi_column import column_boxes
 
 class processPDF:
@@ -14,6 +19,7 @@ class processPDF:
     __outputDir__   = ''
     __workers__     = 0
     __results__     = []
+    __listFiles__   = []
 
     def __init__(self, inputDir, outputDir, workers): 
         self.__inputDir__   = inputDir
@@ -25,13 +31,18 @@ class processPDF:
 
         return glob.glob( directory + '/**/*.pdf', recursive=True)
 
+    def getListFiles (self):
+        return self.__listFiles__
+    
+    #for test:
     @staticmethod
     def processPDFDummy ( path ):
         time.sleep (1)
         return ('el nombre del archivo es %s' % path)
 
+    #simple extract text:
     @staticmethod
-    def processPDF ( path ):
+    def processPDFSimple ( path ):
         doc = fitz.open( path )
         data = ''
         for page in doc:
@@ -41,11 +52,58 @@ class processPDF:
 
             
         return data 
+    
+    @staticmethod
+    def processPDF (path, summary=False ):
+
+        try:
+            # Define the path to the PDF file
+            pdf_file = pathlib.Path (path)
+            
+            # Create a directory to save the extracted content (one directory per PDF file)
+            path_save = pathlib.Path('/export/data_ml4ds/thuban/repository/data/output/') / pdf_file.stem
+            path_save.mkdir(parents=True, exist_ok=True)
+            path_save.joinpath("images").mkdir(parents=True, exist_ok=True)
+            path_save.joinpath("tables").mkdir(parents=True, exist_ok=True)
+            
+            # TODO: This needs to be generic
+            
+            # instructions = \
+            #     """ 
+            #     You are a helpful AI assistant working with technical descriptions of air conditioner units. 
+                
+            #     Please summarize the technical description for the Roof-top air conditioner 680 by sections in such a way that the outputted text can be used as input for a topic modeling algorithm.
+            # """
+            
+            instructions = \
+            """You are a helpful AI assistant working with the generation of summaries of PDF documents. Please summarize the given document by sections in such a way that the outputted text can be used as input for a topic modeling algorithm. Dont start with 'The document can be summarized...' or 'The document is about...'. Just start with the first section of the document.
+            """
+            
+            pdf_parser = PDFParser()
+            
+            if summary:
+                summarizer = Summarizer()      
+                summarizer.summarize(
+                    pdf_file=pdf_file,
+                    instructions=instructions,
+                    path_save=path_save
+                )
+            
+            content = pdf_parser.parse(
+                pdf_path=pdf_file,
+                path_save=path_save)
+            
+            #file ok
+            return ({'path':path,'result':True})
+        except:
+            #file ko
+            return ({'path':path,'result':False})
+              
 
     def processFiles ( self ):
         
         listFiles = self.__getAllDocs (self.__inputDir__)
-
+        self.__listFiles__ = listFiles
 
         num_processes = multiprocessing.cpu_count() if self.__workers__ == 0 else self.__workers__
         print ('using %s workers to process %s files' % (num_processes, len(listFiles)))
@@ -68,10 +126,23 @@ if __name__ == "__main__":
 
     process = processPDF ( args.indir, args.outdir, args.workers)
 
-    #prueba = processPDF.processPDF ('/export/data_ml4ds/thuban/repository/data/Articulos2/2401.15453.pdf')
+    now = datetime.now()
+    start_time = time.time()
     data = process.processFiles ()
+    end_time = time.time()
+    execution_time = end_time - start_time
 
-    import ipdb ; ipdb.set_trace()
+    logdata ={'datetime':now.strftime('%Y-%m-%d'),
+              'executiontime':execution_time //60,
+              'inputdir': args.indir,
+              'numerfiles': len (data),
+              'failedfiles':len ([d for d in data if d['result']==False]),
+              'listfiles': data
+              }
+    with open('/tmp/log.json', 'w') as fp:
+        json.dump(logdata, fp)
+
+
 
 
 
